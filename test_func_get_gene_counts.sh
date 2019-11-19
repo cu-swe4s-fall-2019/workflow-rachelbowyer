da9202acf72ee7339eac575f8dd3638b71ac5f5d
Rachel Bowyer <rabo8409@colorado.edu>
2019-11-19 23:55:29 +0000

    fixed/added functional tests

--- test_func_get_gene_counts.sh
+++ test_func_get_gene_counts.sh
@@ -3,8 +3,12 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-#test that if N is too big insert_..pairs.py doesn't work
+#test that wrong gene name doesn't work
 run gene_name_test python get_gene_counts.py GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz ABCD poop.txt
+assert_exit_code 1
+
+#test that wrong files name doesn't work
+run gene_name_test python get_gene_counts.py poop.gz ABCD poop.txt
 assert_exit_code 1
 
 
---
+++ test_get_tissue_samples.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+#test that wrong tissue group name doesn't work
+run gene_name_test python get_gene_counts.py GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt Bloody poop.txt
+assert_exit_code 1
+
+#test that wrong files name doesn't work
+run gene_name_test python get_gene_counts.py poop.gz Bloody poop.txt
+assert_exit_code 1
+
+
+#test that the output file is created
+rm file.txt
+run gene_name_test python get_gene_counts.py GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt Blood file.txt
+test -f file.txt
